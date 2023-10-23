e59e68f1f322f87f0aec3e3b3aaa3990e86399a2
kitae1995 <kitae1995@naver.com>
2023-10-23 18:17:29 +0900

    1023-STS

--- MyWeb/src/main/java/com/spring/myweb/freeboard/entity/FreeBoard.java
+++ MyWeb/src/main/java/com/spring/myweb/freeboard/entity/FreeBoard.java
@@ -10,29 +10,29 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-/* 
-	CREATE TABLE freeboard(
-	    bno NUMBER PRIMARY KEY,
-	    title VARCHAR2(300) NOT NULL,
-	    writer VARCHAR2(50) NOT NULL,
-	    content VARCHAR2(3000),
-	    reg_date DATE DEFAULT sysdate,
-	    update_date DATE DEFAULT NULL
-	    );
-	    
-	CREATE SEQUENCE freeboard_seq
-	    START WITH 1
-	    INCREMENT BY 1
-	    MAXVALUE 1000
-	    NOCYCLE
-	    NOCACHE;
+/*
+ CREATE TABLE freeboard(
+    bno NUMBER PRIMARY KEY,
+    title VARCHAR2(300) NOT NULL,
+    writer VARCHAR2(50) NOT NULL,
+    content VARCHAR2(3000),
+    reg_date DATE DEFAULT sysdate,
+    update_date DATE DEFAULT NULL
+);
+
+CREATE SEQUENCE freeboard_seq
+    START WITH 1
+    INCREMENT BY 1
+    MAXVALUE 1000
+    NOCYCLE
+    NOCACHE;
  */
+
 @Getter @Setter
 @ToString @EqualsAndHashCode
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-
 public class FreeBoard {
 	
 	private int bno;
@@ -43,3 +43,16 @@ public class FreeBoard {
 	private LocalDateTime updateDate;
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
